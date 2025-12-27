6414f97cd466f38a2e8182af1eb3eb2c152589bb
Prakhar Patel <prakhar.codes433@gmail.com>
2025-12-27 09:07:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,10 +53,10 @@ function showToast() {
   const toast = document.getElementById("snackbar");
   toast.className = "show";
   
+
   setTimeout(function() {
     toast.className = toast.className.replace("show", "");
   }, 3000);
 }
-
 
 
